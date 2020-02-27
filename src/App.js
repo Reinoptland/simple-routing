4ccab634e561aa5284e897b69ff9624430b13706
Reinoptland <reinoptland@gmail.com>
2020-02-27 16:57:10 +0100

    simple routing example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+
+function HomePage() {
+  return <h1>HOME!</h1>;
+}
+
+function AboutPage() {
+  return (
+    <div>
+      <h2>ABOUT... </h2>
+      <Link to="/"> Go back home </Link>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <Link to="/">HOME</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Switch>
+          <Route path="/" exact={true} component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+        </Switch>
       </header>
     </div>
   );
